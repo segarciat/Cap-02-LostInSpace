2102c2282d51141084cd1511b7b5490d0f0753e5
Michael Greene <mgreene11111@yahoo.com>
2023-01-26 12:25:39 -0800

    Merged main files. Renamed Main class to App.

--- src/main/java/com/lostinspace/app/App.java
+++ src/main/java/com/lostinspace/app/App.java
@@ -11,26 +11,27 @@ import com.lostinspace.classes.Room;
 import com.lostinspace.classes.RoomsRoot;
 import com.lostinspace.util.Controller;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
 import java.util.*;
 
-public class DevMain {
+public class App {
     public static String currentRoom = new String();         // current location of Player
     private static Controller controller = new Controller(); // make an instance of controller for player commands
     private static RoomsRoot testMap = new RoomsRoot();      // create an instance of the game map
     private static ArrayList<Room> map;                             // ref to testMap.rooms
     // GAME LOGIC
     public static void main(String[] args) throws IOException {
+        // todo multi-threading for delays in text
+
+        titleCard();                                         // display title card
         currentRoom = "Docking Bay";                         // start the player in the Docking Bay
         System.out.println(controller.showInstructions());   // display instructions
 
         // try to load the game map
         testMap = controller.loadMap();                      // load the map into memory
         map = testMap.rooms;
-
-        // todo .loadMap() tests - DELETE ONCE TEAM HAS APPROVED BRANCH
-        System.out.println("Name: " + map.get(1).name + "  Exit: " + map.get(1).exits.getEast());
-        System.out.println("Name: " + map.get(0).name + "  Exit: " + map.get(0).exits.getWest());
 
 
         // breaking this while loop means the game is over
@@ -62,7 +63,8 @@ public class DevMain {
                 // movement commands
                 if (inputArr[0].equals("go") || inputArr[0].equals("walk") || inputArr[0].equals("move") || inputArr[0].equals("run")) {
                     // check that player is allowed to go in that direction
-                    currentRoom = move(currentRoom, inputArr[1]);
+                    currentRoom = controller.move(testMap, currentRoom, inputArr[1]);
+
 
                 }
 
@@ -70,54 +72,53 @@ public class DevMain {
                 if(inputArr[0].equals("exit") || inputArr[0].equals("quit") || inputArr[0].equals("escape")){
                     System.exit(0);
                 }
+
+                // restart the game
+                if(inputArr[0].equals("new") || inputArr[0].equals("restart") || inputArr[0].equals("escape")){
+                    restart();
+                }
             }
         }
     }
 
-    // moves player between rooms in map
-    // returns string which resets currentRoom
-    public static String move(String room, String dir) {
-        String retRoom = ""; // create empty string to hold return room
-
-        // iterate through map
-        for(int i = 0; i < map.size(); i++){
-            // if the direction desired exists as an exit in that room...
-            if(map.get(i).name.equals(room)){
-                // ...reassign retRoom as the room in that direction
-                switch (dir){
-                    case "north":
-                        retRoom = map.get(i).exits.getNorth();
-                        break;
-
-                    case "south":
-                        retRoom = map.get(i).exits.getSouth();
-                        break;
-
-                    case "east":
-                        retRoom = map.get(i).exits.getEast();
-                        break;
-
-                    case "west":
-                        retRoom = map.get(i).exits.getWest();
-                        break;
-                    // if an invalid direction is chosen, tell the player
-                    default:
-                        System.out.println("\nINVALID DIRECTION: " + dir);
-                        System.out.println("\nChoose a valid direction. (Hint: INSPECT ROOM if you're lost)");
-                        retRoom = room;
-                        break;
-                }
-
-                // if retRoom is an empty string then there is no exit in that direction
-                if(retRoom.equals("")){
-                    System.out.println("\nINVALID DIRECTION: " + dir);
-                    System.out.println("\nThere is no EXIT in that DIRECTION. (Hint: INSPECT ROOM if you're lost)");
-                    retRoom = room;
-
-                    return retRoom; // return back to starting room
-                }
-            }
+    // restarts game when called
+    public static void restart(){
+        String[] string = {};
+        try {
+            main(string);
+        } catch (IOException err){
+            err.printStackTrace();
         }
-        return retRoom; // return new room
+    }
+
+
+
+    static String titleCard() {
+        String content = ""; // empty return string
+
+        try {
+            // load file from resources dir
+            BufferedReader reader = new BufferedReader(new FileReader("data/scripts/title.txt"));
+
+            StringBuilder sB = new StringBuilder();            // sB builds title card line by line
+            String line = null;                                // empty string for line
+            String ls = System.getProperty("line.separator");  // line separator
+
+            // while there are still lines of characters to read
+            while ((line = reader.readLine()) != null) {
+                sB.append(line);                    // append the next line to the SB
+                sB.append(ls);                      // new line
+            }
+
+            sB.deleteCharAt(sB.length() - 1);       // delete the last new line separator
+            reader.close();                         // close file being worked with
+            content = sB.toString();         // create new string with sB content
+            System.out.println(content);            // display title card!
+
+        } catch (IOException err) {                 // throw IO Exception if failed
+            err.printStackTrace();
+        }
+
+        return content;
     }
 }
--- src/main/java/com/lostinspace/classes/Room.java
+++ src/main/java/com/lostinspace/classes/Room.java
@@ -13,4 +13,20 @@ public class Room {
     public String description;      // description of area
     public ArrayList<String> items; // array of items that can be used/got in the currentRoom
     public Exit exits;              // object acts as where each direction will lead player
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public ArrayList<String> getItems() {
+        return items;
+    }
+
+    public Exit getExits() {
+        return exits;
+    }
 }
