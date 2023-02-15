c99b7395797952d8c5cbc1eb93b0b82629c27b2c
wrijlarr <wrijlarr@amazon.com>
2023-02-15 12:56:48 -0500

    commenting in code classes AB#1353

--- src/main/java/com/lostinspace/view/OpenDialogAction.java
+++ src/main/java/com/lostinspace/view/OpenDialogAction.java
@@ -6,11 +6,13 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class OpenDialogAction implements ActionListener {
+
+    // window settings, font text, font color
     private static final int WINDOW_SIZE = 720;
     private static final Font MONOSPACE_PLAIN_MED = new Font("Monospaced", Font.PLAIN, 14);
-    // font colors
     private static final java.awt.Color COLOR_GREEN = new Color(76, 175, 82);
 
+    // fields for the frame, text area, title, and buttons
     private JFrame frame;
     private JTextArea textArea = new JTextArea();
     private String title;
@@ -30,7 +32,7 @@ public class OpenDialogAction implements ActionListener {
         });
     }
 
-
+    /// actions for image settings in the dialog frame
     @Override
     public void actionPerformed(ActionEvent e) {
         ImagePanel imgPanel = new ImagePanel("/images_title/background.jpg", 720, 720);
@@ -43,8 +45,8 @@ public class OpenDialogAction implements ActionListener {
 
     }
 
+    // Set text area attributes
     private static void setTextAreaOptions(JTextArea textArea, String text) {
-        // Set text area attributes
         textArea.setText(text);                        // initial text
         textArea.setSize(WINDOW_SIZE, WINDOW_SIZE);                          // size
         textArea.setEditable(false);                                         // non-editable
