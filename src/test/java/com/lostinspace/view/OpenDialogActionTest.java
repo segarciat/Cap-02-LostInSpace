5e7491b3d9566faaf5210a142dc97b901a5caaf6
wrijlarr <wrijlarr@amazon.com>
2023-02-17 13:55:27 -0500

    Edit Text dialogue for README AB#1420

---
+++ src/test/java/com/lostinspace/view/OpenDialogActionTest.java
@@ -0,0 +1,16 @@
+package com.lostinspace.view;
+
+import junit.framework.TestCase;
+import org.junit.Test;
+
+
+public class OpenDialogActionTest extends TestCase {
+
+    @Test
+    public void actionPerformed() {
+
+    // Action performed method
+
+
+    }
+}
