d5ae2d0d0f9e199d20307a8451123c5f9efae5bf
Sergio Garcia <segarciat314@gmail.com>
2023-02-17 13:51:25 -0500

    AB#1393, added boundary value test for pipes and a test to make sure pipes cannot be used multiple times.

--- src/test/java/com/lostinspace/controller/GUIConsoleControllerTest.java
+++ src/test/java/com/lostinspace/controller/GUIConsoleControllerTest.java
@@ -30,4 +30,35 @@ public class GUIConsoleControllerTest {
 
         assertEquals(player.getOxygen(), 25, 0.0001);
     }
+
+    @Test
+    public void interactItem_whenItemIsPipes_andPipesHaveAlreadyBeenUsed_doNotReplenishOxygen() {
+        // Set player oxygen to 0 to test that oxygen increases by the expected amount.
+        Player player = controller.getPlayer();
+        player.setOxygen(0);
+
+        ItemMod pipes = controller.getModel().getItems().get("pipes");
+
+        // Oxygen goes up to 25.
+        controller.interactItem(pipes);
+
+        // Oxygen should stay at 25, since pipes have already been used.
+        controller.interactItem(pipes);
+
+        assertEquals(player.getOxygen(), 25, 0.0001);
+    }
+
+    @Test
+    public void interactItem_whenItemIsPipes_playerOxygenShouldNotExceedMaximum() {
+        double maximumOxygen = 100.0;
+        Player player = controller.getPlayer();
+        player.setOxygen(maximumOxygen);
+
+        ItemMod pipes = controller.getModel().getItems().get("pipes");
+
+        // Oxygen should not exceed the maximum.
+        controller.interactItem(pipes);
+
+        assertEquals(player.getOxygen(), maximumOxygen, 0.0001);
+    }
 }
