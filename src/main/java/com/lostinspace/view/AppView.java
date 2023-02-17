a102655b7dc657d4088267d9374ede176c9d73df
Sergio Garcia <segarciat314@gmail.com>
2023-02-17 12:11:55 -0500

    AB#1405, using the scrambler will now remove the monster from the room

--- src/main/java/com/lostinspace/view/AppView.java
+++ src/main/java/com/lostinspace/view/AppView.java
@@ -179,7 +179,9 @@ public class AppView {
         return controller;
     }
 
-
+    public RoomPanel getCurrentRoomPanel() {
+        return currentRoomPanel;
+    }
 
     /**
      * Allows player to toggle (open/close) the menu when pressing ESC key.
